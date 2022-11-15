c22b16169bb886cb27f4f07bf61028f080e9abe5
MikusSaule <109538364+MikusSaule@users.noreply.github.com>
2022-11-15 22:10:42 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i = 1;
+        while (i < 6) {
+            System.out.println("Timer:" + i);
+            i ++;
+            
+
+        }
+
+        System.out.println("Boom!");
     }
 }
